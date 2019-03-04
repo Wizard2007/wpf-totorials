4d9dc61d58b25676fe31bae542e060f8ae0be985
Eujine <sponsor85@mail.ru>
2019-03-04 16:41:25 +0200

    [+] Hooking Up Views

---
+++ HookingUpViews/HookingUpViews/HookingUpViewsCodeFirst/FirstApp/Model/Student.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel;
+
+namespace FirstApp.Model
+{
+
+    public class StudentModel { }
+
+    public class Student : INotifyPropertyChanged
+    {
+        private string _firstName;
+        private string _lastName;
+
+        public string FirstName
+        {
+            get => _firstName;
+
+            set
+            {
+                if (_firstName != value)
+                {
+                    _firstName = value;
+                    RaisePropertyChanged("FirstName");
+                    RaisePropertyChanged("FullName");
+                }
+            }
+        }
+
+        public string LastName
+        {
+            get => _lastName; 
+
+            set
+            {
+                if (_lastName != value)
+                {
+                    _lastName = value;
+                    RaisePropertyChanged("LastName");
+                    RaisePropertyChanged("FullName");
+                }
+            }
+        }
+
+        public string FullName => $"{_firstName} {_lastName}";
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string property)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
---
+++ HookingUpViews/HookingUpViews/HookingUpViewsCodeFirst/FirstApp/ViewModel/StudentViewModel.cs
@@ -0,0 +1,19 @@
+﻿using FirstApp.Model;
+using System.Collections.ObjectModel;
+
+namespace FirstApp.ViewModel
+{
+    public class StudentViewModel
+    {
+        public StudentViewModel() => LoadStudents();
+        public ObservableCollection<Student> Students { get; set; }
+
+        public void LoadStudents() =>
+            Students = new ObservableCollection<Student>
+            {
+                new Student { FirstName = "Mark", LastName = "Allain" },
+                new Student { FirstName = "Allen", LastName = "Brown" },
+                new Student { FirstName = "Linda", LastName = "Hamerski" }
+            };
+    }
+}
