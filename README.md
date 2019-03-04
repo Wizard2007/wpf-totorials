fe2e409e1ba69fa06f95cf9c22006c117857c7e7
Eujine <sponsor85@mail.ru>
2019-03-05 00:48:23 +0200

    [+] WPF DataBinding

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # wpf-totorials
 wpf tutotrials
+https://www.tutorialspoint.com/mvvm/mvvm_hooking_up_viewmodel.html
+https://www.tutorialspoint.com/wpf/
---
+++ WPFDataBindings/FirstApp/ViewModel/StudentViewModel.cs
@@ -0,0 +1,18 @@
+﻿using FirstApp.Model;
+using System.Collections.ObjectModel;
+
+namespace FirstApp.ViewModel
+{
+    public class StudentViewModel
+    {
+        public ObservableCollection<Student> Students { get; set; }
+
+        public void LoadStudents() =>
+            Students = new ObservableCollection<Student>
+            {
+                new Student { FirstName = "Mark", LastName = "Allain" },
+                new Student { FirstName = "Allen", LastName = "Brown" },
+                new Student { FirstName = "Linda", LastName = "Hamerski" }
+            };
+    }
+}
